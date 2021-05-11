e5e51e2d1f645209bce88ef21b168586df7d9aa1
chrisweeting <chrisweeting@gmail.com>
2021-05-10 21:28:47 -0400

    Session form

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -22,13 +22,13 @@ const receiveErrors = (errors) => ({
 export const signup = (user) => (dispatch) => (
   postUser(user).then(user => 
     dispatch(receiveCurrentUser(user))
-  ), err => (dispatch(receiveErrors(err.responseJSON)))
+  ).fail(err => (dispatch(receiveErrors(err.responseJSON))))
 );
 
 export const login = (user) => (dispatch) => (
   postSession(user).then(user => 
     dispatch(receiveCurrentUser(user))
-  ), err => (dispatch(receiveErrors(err.responseJSON)))
+  ).fail(err => (dispatch(receiveErrors(err.responseJSON))))
 );
 
 export const logout = () => (dispatch) => (
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SigninFormContainer from './session/signin_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -11,8 +12,8 @@ const App = () => (
     </Link>
 
     <GreetingContainer />
-    <Route exact path='/signin' component={SigninFormContainer} ></Route>
-    <Route exact path='/signup' component={SignupFormContainer} ></Route>
+    <AuthRoute exact path='/signin' component={SigninFormContainer} ></AuthRoute>
+    <AuthRoute exact path='/signup' component={SignupFormContainer} ></AuthRoute>
   </div>
 );
 
